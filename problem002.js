f1ecb3e43aaad936bc6fa6997393cbafbca72f40
MrHidee <44234452+MrHidee@users.noreply.github.com>
2018-11-28 16:12:57 +0100

    Add files via upload

--- problem002.js
+++ problem002.js
@@ -1,6 +1,15 @@
-
 function stringRotate(a, b) {
-	return a.slice(b) + a.slice(0, b)
-
+  if(isNaN(b) || b<0)
+    return "";
+  else {
+    var array = a.split("");
+    var d = array.length;
+    for(var i=0;i<b;i++){
+      var n = array[0];
+      array.shift();
+      array.push(n);
+    }
+    array = array.join("");
+    return array;
+  }
 }
-
